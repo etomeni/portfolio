73ed833373c35bb3f79d5a489ca4975a378b0f35
etomeni <sundaywht@gmail.com>
2021-09-03 09:44:10 +0100

    pdf created, project complete

--- authController/config/constant.php
+++ authController/config/constant.php
@@ -1,15 +1,15 @@
 <?php
 
-    // //Localhost mySQL connection database
-    // define ('dbHostName', 'localhost');
-    // define ('dbUserName', 'uutms');
-    // define ('dbPassword', 'pastor@1');
-    // define ('dbName', 'hnginternship');
+    //Localhost mySQL connection database
+    define ('dbHostName', 'localhost');
+    define ('dbUserName', 'uutms');
+    define ('dbPassword', 'pastor@1');
+    define ('dbName', 'hnginternship');
 
-    //Remote mySQL connection database
-    define ('dbHostName', 'remotemysql.com');
-    define ('dbUserName', 'pMDdFNUYdp');
-    define ('dbPassword', 'VnmDP5Ll7i');
-    define ('dbName', 'pMDdFNUYdp');
+    // //Remote mySQL connection database
+    // define ('dbHostName', 'remotemysql.com');
+    // define ('dbUserName', 'pMDdFNUYdp');
+    // define ('dbPassword', 'VnmDP5Ll7i');
+    // define ('dbName', 'pMDdFNUYdp');
 
 ?>
