6806ed9ca780d8e2cabd96ace3a575326c1aa724
etomeni <sundaywht@gmail.com>
2021-09-23 12:58:07 +0100

    reduce the image size and changed MySql connection

--- authController/config/constant.php
+++ authController/config/constant.php
@@ -6,10 +6,16 @@
     define ('dbPassword', 'pastor@1');
     define ('dbName', 'hnginternship');
 
-    // //Remote mySQL connection database
+    // //RemoteMySQL.Com connection database
     // define ('dbHostName', 'remotemysql.com');
     // define ('dbUserName', 'pMDdFNUYdp');
     // define ('dbPassword', 'VnmDP5Ll7i');
     // define ('dbName', 'pMDdFNUYdp');
+
+    // //app.infinityfree.net connection database
+    // define ('dbHostName', 'sql110.epizy.com');
+    // define ('dbUserName', 'epiz_29441082_w197');
+    // define ('dbPassword', 'aUVc9dzAgk5');
+    // define ('dbName', 'epiz_29441082_w197');
 
 ?>
